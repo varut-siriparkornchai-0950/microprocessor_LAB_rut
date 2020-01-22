ac4fa87c674154f932d36c454d07bd399e5fa04d
varut siriparkornchai <61010950@kmitl.ac.th>
2020-01-23 01:07:13 +0700

    Update assignment_2_MICROLAB.ino

--- assignment_2_MICROLAB/assignment_2_MICROLAB.ino
+++ assignment_2_MICROLAB/assignment_2_MICROLAB.ino
@@ -7,37 +7,39 @@ void setup()
   DDRD=0b11111100;         
   DDRB=0b00000011;  
   Serial.begin(9600);   
-  ledOn(0); 
+  LEDup(0); 
 }
 
 void loop()
 {
   a0=digitalRead(A0);
   a1=digitalRead(A1);
-  if(p>7)             //reset go left
+  if(p>7)                                   //go throught left
   {
     p = 0;
   }
-  else if(p<0)      
+  else if(p<0)                              //go through right
   {
     p = 7;
   }
-  ledOn(p);
+  LEDup(p);                                 //turn-on LED
 
-  if((a0 == 1)&&(check0 == "open"))
+//     _________Door of filter clicker_________
+
+  if((a0 == 1)&&(check0 == "open"))         //A0 button   DOOR open & finger up
   {
     check0 = "close";
   }
-  else if((a0 == 0)&&(check0 == "close"))
+  else if((a0 == 0)&&(check0 == "close"))   // DOOR close & finger down
   {
     p++;
     check0 = "open";
   }
-  else if((a1 == 1)&&(check1 =="open"))
+  else if((a1 == 1)&&(check1 =="open"))      //A1 button   DOOR open & finger up 
   {
     check1 = "close";
   }
-  else if((a1 == 0)&&(check1 == "close"))
+  else if((a1 == 0)&&(check1 == "close"))   // DOOR close & finger down
   {
     p--;
     check1 = "open";
@@ -47,16 +49,16 @@ void loop()
  Serial.print(a0);
  Serial.println(a1);
  Serial.println(p);
-  Serial.println(check0);
-    Serial.println(check1);
+ Serial.println(check0);
+ Serial.println(check1);
 
 
 }
 
-void ledOn(int position_LED)
+void LEDup(int position_LED)                //function shift position
 {
   byte LED_D=0b00000100;
-  byte LED_B=0b000001;
+  byte LED_B=0b00000001;
   if(position_LED>=0 && position_LED<=5)
   {
     PORTB = 0x00;
