b6b548a51618edae15ce4bd627d56297191ce97c
varut siriparkornchai <61010950@kmitl.ac.th>
2020-01-22 22:10:31 +0700

    update

--- assignment_1_MICROLAB/assignment_1_MICROLAB.ino
+++ assignment_1_MICROLAB/assignment_1_MICROLAB.ino
@@ -1,23 +1,25 @@
-void setup() {
+void setup() 
+{
   DDRD=0xFF;
   DDRB=0xFF;
-  Serial.begin(9600);
 }
 
-void loop() {
+void loop() 
+{
  PORTD=0b00000100;
- PORTB=0b000000;
- delay(500);
- for(int b=0;b<=4;b++){
+ PORTB=0b00000000;
+ delay(200);
+ for(int b=0;b<=4;b++)
+ {
    PORTD=PORTD<<1;
-   delay(500);
-   Serial.print(b);
-   if(b>=4){
+   delay(200);
+   if(b>=4)
+   {
      PORTD=0x00;
-     PORTB=0b000001;
-     delay(500);
+     PORTB=0b00000001;
+     delay(200);
      PORTB=PORTB<<1;
-     delay(500);
+     delay(200);
    }
  }
 }
--- assignment_2_MICROLAB/assignment_2_MICROLAB.ino
+++ assignment_2_MICROLAB/assignment_2_MICROLAB.ino
@@ -1,14 +1,34 @@
 void setup()
 {
-  DDRD=0xFF;
-  DDRB=0xFF;
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  DDRD=0xFC;          //0b1111 1100
+  DDRB=0xFC;          //ob1111 1100
   Serial.begin(9600);
 }
 
 void loop()
 {
-  PORTD=0b00000100;
- PORTB=0b000000;
- delay(500);
+  int a0=digitalRead(A0);
+  int a1=digitalRead(A1);
+  delay(500);
+
+  ledOn(1);
  
+}
+
+void ledOn(int position_LED)
+{
+  byte LED_D=0b00000100;
+  byte LED_B=0b00000001;
+  if(position_LED>=0 && position_LED<=5)
+  {
+    PORTB = 0x00;
+    PORTD = LED_D << position_LED;
+  }
+  else if(position_LED ==6 || position_LED ==7)
+  {
+    PORTD = 0x00;
+    PORTB = LED_B << (position_LED % 6);
+  }
 }
