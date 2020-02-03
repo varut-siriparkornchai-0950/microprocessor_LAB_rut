42ace48d5137e5f0ed77c299ec70d350133acf68
varut siriparkornchai <61010950@kmitl.ac.th>
2020-02-03 14:21:54 +0700

    update wk3

---
+++ week_3/assignment_1_wk3/assignment_1_wk3.ino
@@ -0,0 +1,40 @@
+int clk_pin = 13; //SHCP
+int latch_pin = 10; //STCP
+int data_pin = 11; //DS
+byte number[] = {0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
+int a=0, num;
+void setLed(uint8_t _state)
+{
+  digitalWrite(latch_pin,LOW);
+  for(int i = 0 ; i < 8 ; i++){
+  digitalWrite(data_pin,(_state >> (7-i)) & 0x01);
+  digitalWrite(clk_pin,HIGH);
+  digitalWrite(clk_pin,LOW);
+}
+  digitalWrite(latch_pin,HIGH);
+}
+
+void setup() {
+  pinMode(clk_pin,OUTPUT);
+  pinMode(latch_pin,OUTPUT);
+  pinMode(data_pin,OUTPUT);
+  Serial.begin(9600);
+  setLed(0x3F);
+}
+void loop() {
+  if(Serial.available()>0)
+  {
+    num = Serial.read();          //input 0 = 48 (ascii)
+    playnum(num - 48);
+    Serial.println(num);
+  }
+  
+  
+  
+}
+
+void playnum(int pos)
+{
+  setLed(number[pos]);
+  delay(1000);
+}
