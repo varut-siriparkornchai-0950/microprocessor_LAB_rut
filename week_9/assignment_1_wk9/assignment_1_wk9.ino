e7548a700ebc7d94f19f70d859237e03fb8a4116
varut <varut_99@hotmail.com>
2020-03-30 00:54:17 +0700

    wk9

---
+++ week_9/assignment_1_wk9/assignment_1_wk9.ino
@@ -0,0 +1,35 @@
+#define LED1 11
+#define INT_PIN 3
+
+int timedelay=1000, mode=1;
+
+void ISR_Callback()           // come here when INT_PIN was not avariable(not emptry)
+{
+  if(mode>=3)
+  {
+    mode=1;
+    timedelay = 0;
+  }
+  timedelay = timedelay+1000;
+  mode++;
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED1, OUTPUT);
+  pinMode(INT_PIN, INPUT);
+  
+  attachInterrupt(digitalPinToInterrupt(INT_PIN), 
+                            ISR_Callback, RISING);    // RISING(0v -> 5v) or FALLING(5v -> 0v) or CHANGE(0v -> 5v or 5v -> 0v)
+}
+
+void loop()
+{
+  digitalWrite(LED1, HIGH);
+  delay(timedelay);
+  digitalWrite(LED1, LOW);
+  delay(timedelay);
+  
+  Serial.println(timedelay);
+}
---
+++ week_9/assignment_2_wk9/assignment_2_wk9.ino
@@ -0,0 +1,65 @@
+#define LED1 11
+#define INT_PIN 3
+bool toggle;
+int mode=1, timer1_counter, sw, countRound=1;
+String check0 = "open"; 
+
+ISR(TIMER1_OVF_vect)
+{
+  if((mode == 1)&& (countRound == 1))
+  {
+    TCNT1 = timer1_counter;
+    toggle = !toggle;
+  }
+  else if((mode == 2)&& (countRound == 2))
+  {
+    TCNT1 = timer1_counter;
+    toggle = !toggle;
+  }
+  else if((mode == 3)&& (countRound == 3))
+  {
+    TCNT1 = timer1_counter;
+    toggle = !toggle;
+  }
+  else{
+    countRound++;
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED1, OUTPUT);
+  pinMode(INT_PIN, INPUT);
+  
+  noInterrupts();         
+  TCCR1A = 0;       //reset timer
+  TCCR1B = 0;       //reset timer
+
+  timer1_counter = 1;
+  TCNT1 = timer1_counter;
+  TCCR1B |= (1 << CS12);
+  TIMSK1 |= (1 << TOIE1);
+
+  interrupts();
+}
+
+void loop()
+{
+  sw = digitalRead(INT_PIN);
+  if((sw == 1)&&(check0 == "open"))         //A0 button   DOOR open & finger up
+  {
+    check0 = "close";
+  }
+  else if((sw == 0)&&(check0 == "close"))   // DOOR close & finger down
+  {
+    if(mode == 3){
+      mode = 0;
+      countRound = 1;
+    }
+    mode++;
+    check0 = "open";
+  }
+  Serial.println(mode);
+  digitalWrite(LED1, toggle);
+}
